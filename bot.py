eee4fc04de4feee25f09226f9d9d8a17d747d5ee
Lukwok <45985935+Lukwok@users.noreply.github.com>
2021-06-11 21:00:18 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
-PORT = int(os.environ.get('PORT', 5000))
+PORT = int(os.environ.get('PORT', 5000))    
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -18,7 +18,24 @@ def start(update, context):
 
 def help(update, context):
     """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
+    output = """皆さん~ 路過sansan 教你點用 <3 \n
+    注意!! 此乃路過一日亂整出黎! 請不要同人分享或自己亂玩\n
+    所有資料被第三方改動,路過不會負責~~ (我懶)\n
+    1. /start \t 唔好亂用 !!佢會restart個bot同清空record!!\n
+    2. /join [name] \t 如果得閒打族戰就報名la\n
+    3. /sorry [name] \t Sor9ly, 是日要陪女陪腦細,打唔到 QWQ\n
+    4. /delete [name] \t 哎? ざんねん~ 原來我都係唔得閒打.. \n
+    5. /fuck [name] \t 頂,終於做完野~~可以打得~~\n
+    6. /show \t 出結果\n
+    7. /close \t 898 \n
+    8. /help \t 召喚我 \n\n
+    仲有冇咩唔明? 但就算有我都幫你唔到 :P """
+    update.message.reply_text(output)
+
+def join(bot,update):
+    id = update.message.chat.id
+    output = "halo"+update.message.from_user.full_name+", your id is"+id
+    bot.send_message(id,output)
 
 def echo(update, context):
     """Echo the user message."""
@@ -41,6 +58,7 @@ def main():
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("join", join))
 
     # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, echo))
