b28ddb7d5b80874c0f58172b9d266f0fdeb30afd
Lukwok <45985935+Lukwok@users.noreply.github.com>
2021-06-13 14:25:46 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -125,7 +125,7 @@ def main():
     # dp.add_handler(MessageHandler(Filters.text, echo))
 
     # log all errors
-    dp.add_error_handler(error)
+    #dp.add_error_handler(error)
 
     # Start the Bot
     updater.start_webhook(listen="0.0.0.0",
