452994d74c4f85ed87211d7666dcf3de167db4c8
Lukwok <45985935+Lukwok@users.noreply.github.com>
2021-10-10 00:52:39 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-TOKEN = 'YOUR_TOKEN
+TOKEN = 'YOUR_TOKEN'
 
 #variable list
 avaList = []
